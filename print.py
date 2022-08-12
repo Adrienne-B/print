ae2cd657b26da2f69a6ec3830e5d622176b2daf6
Adrienne Burney <80119637+Adrienne-B@users.noreply.github.com>
2022-08-12 08:55:43 -0500

    Add files via upload

---
+++ print.py
@@ -0,0 +1,15 @@
+#print(2+2) #This prints text
+
+#python_student ="Adrienne"
+#python_student = 262+
+#print(python_student)
+
+python_student = input("What is your job occupation?")
+
+#Unlike Javascript and Java..in Python you do not need a semicolon
+#or console.logsl
+#You do not need to put let, var, or const
+
+
+#
+
